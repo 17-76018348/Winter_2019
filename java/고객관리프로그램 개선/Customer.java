e13a3734419b0df7cfd35e75fb262d7a0a1c292e
17-76018348 <53898123+17-76018348@users.noreply.github.com>
2020-01-08 16:17:01 +0900

    20200108

---
+++ java/고객관리프로그램 개선/Customer.java
@@ -0,0 +1,51 @@
+package homework1;
+
+public class Customer {
+	private String name;
+	private String gender;
+	private int point;
+	private int password;
+	public Customer()
+	{
+		this.name = null;
+		this.gender = null;
+		this.point = 0;
+		this.password = -9999;
+	}
+
+	
+	public int getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+}
