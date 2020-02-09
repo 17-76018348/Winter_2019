0ac2d1014fe56123f32d658c8e0afe745c6f3d48
17-76018348 <53898123+17-76018348@users.noreply.github.com>
2020-02-09 21:53:31 +0900

    0209

--- java/연습/2019_winter/src/chattingServer/Constants.java
+++
@@ -1,8 +0,0 @@
-package chattingServer;
-
-public interface Constants {
-	public static final String CONNECTFAIL = "연결 끊김";
-	public static final String CLIENTNAME = "클라이언트:  ";
-	public static final String SERVERNAME = "서버:  ";
-	public static final String EXIT = "exit";
-}
